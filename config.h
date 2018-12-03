28165982c2166fa03087ac804e80f2a4df53d26e
lilipach <45573781+lilipach@users.noreply.github.com>
2018-12-03 12:41:22 -0800

    Update config.h

--- config.h
+++ config.h
@@ -1,9 +1,9 @@
 /**
- * Linked List class (Lab 5) configuration file.
+ * Linked List class configuration file.
  * Activate test #N by defining the corresponding LAB5_TESTN to have the value 1.
  */
 
 #define LAB5_TEST1	1	// 1 means test with int instead of char
-#define LAB5_TEST2	1	// Activate moveToBeginning (prog exercise 2)
-#define LAB5_TEST3	1	// Activate insertBefore (prog exercise 3)
+#define LAB5_TEST2	1	// Activate moveToBeginning 
+#define LAB5_TEST3	1	// Activate insertBefore
 
